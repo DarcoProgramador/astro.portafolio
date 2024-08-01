d2af7bca65d59f1fa34b25e76ca7f996d871ff57
DarcoProgramador <denangel123@gmail.com>
2024-08-01 15:10:20 -0600

    astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: "https://git/",
+  site: "https://darcoprogramador.github.io",
+  base: "/astro.portafolio",
 });
